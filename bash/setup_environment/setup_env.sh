ff75ea450808779a8abdf1c7fe170e2194187466
Charles GAYDON <charles.gaydon@gmail.com>
2021-09-23 13:01:47 +0200

    Refactor setup env to have clean pytorch geometric install

--- bash/setup_environment/setup_env.sh
+++ bash/setup_environment/setup_env.sh
@@ -4,7 +4,15 @@ conda install mamba -n base -c conda-forge
 mamba env create -f bash/setup_environment/requirements.yml
 conda activate validation_module
 
-# pip install torch==1.8.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html  # if pytorch extensions are needed, use this torch install
+mamba install pyg -c pyg -c conda-forge
+mamba install -c conda-forge pytorch-lightning
+mamba install torchvision>=0.9.1 -c pytorch  # not working
+
+
+
+# DEPRECATED
+# mamba install pytorch==1.9.1+cu111 torchvision>=0.9.1 cudatoolkit=11.1 -c pytorch -c nvidia
+# pip install torch==1.9.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html  # if pytorch extensions are needed, use this torch install
 # pip install pytorch-lightning
 # pip install torchnet==0.0.4
-# pip install -r setup_environment/torch_extensions.txt --find-links https://pytorch-geometric.com/whl/torch-1.8.0+cu111.html
+# pip install -r bash/setup_environment/torch_extensions.txt --find-links https://pytorch-geometric.com/whl/torch-1.8.0+cu111.html
