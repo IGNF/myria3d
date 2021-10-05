93635d402f34ce5ebc93ae56fab3201b729910c1
Charles GAYDON <charles.gaydon@gmail.com>
2021-10-05 12:15:29 +0200

    Add option to output confusion of predictions

--- semantic_val/models/point_net_model.py
+++ semantic_val/models/point_net_model.py
@@ -32,6 +32,10 @@ class PointNetModel(LightningModule):
 
     Read the docs:
         https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+
+    :param save_predictions: Set to True to save LAS files with predictions as classification field.
+    :param confusion_mode: Set to True to save difference of preds with targets classes (0: TN, 1:FP, 2:FN, 3:TP).
+    Only in effect if save_predictions is True.
     """
 
     def __init__(
@@ -43,6 +47,7 @@ class PointNetModel(LightningModule):
         subsampling_size: int = 30000,
         lr: float = 0.001,
         save_predictions: bool = False,
+        confusion_mode: bool = True,
     ):
         super().__init__()
 
@@ -52,9 +57,9 @@ class PointNetModel(LightningModule):
         self.model = Net(hparams=self.hparams)
 
         self.save_predictions = save_predictions
+        self.confusion_mode = confusion_mode
         self.in_memory_tile_id = ""
 
-        # TODO: parametrize : https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html?highlight=crossentropyloss#
         self.softmax = nn.Softmax(dim=1)
         self.criterion = torch.nn.CrossEntropyLoss(weight=torch.FloatTensor([0.2, 1.0]))
         self.train_iou = IoU(num_classes, reduction="none")
@@ -79,21 +84,23 @@ class PointNetModel(LightningModule):
 
     def training_step(self, batch: Any, batch_idx: int):
         loss, _, _, preds, targets = self.step(batch)
-        preds_avg = (preds * 1.0).mean().item()
-        targets_avg = (targets * 1.0).mean().item()
-        log.debug(f"Train batch building % = {targets_avg}")
+
         acc = self.train_accuracy(preds, targets)
         iou = self.train_iou(preds, targets)[1]
         self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
         self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/iou", iou, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("train/preds_avg", preds_avg, on_step=True, on_epoch=True, prog_bar=True)
+
+        preds_avg = (preds * 1.0).mean().item()
+        targets_avg = (targets * 1.0).mean().item()
+        log.debug(f"Train batch building % = {targets_avg}")
+        self.log("train/preds_avg", preds_avg, on_step=True, on_epoch=True, prog_bar=False)
         self.log(
             "train/targets_avg",
             targets_avg,
             on_step=True,
             on_epoch=True,
-            prog_bar=True,
+            prog_bar=False,
         )
 
         # we can return here dict with any tensors
@@ -108,21 +115,22 @@ class PointNetModel(LightningModule):
     def validation_step(self, batch: Any, batch_idx: int):
         loss, _, proba, preds, targets = self.step(batch)
 
-        preds_avg = (preds * 1.0).mean().item()
-        targets_avg = (targets * 1.0).mean().item()
         acc = self.val_accuracy(preds, targets)
         iou = self.val_iou(preds, targets)[1]
 
         self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
         self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
         self.log("val/iou", iou, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val/preds_avg", preds_avg, on_step=True, on_epoch=True, prog_bar=True)
+
+        preds_avg = (preds * 1.0).mean().item()
+        targets_avg = (targets * 1.0).mean().item()
+        self.log("val/preds_avg", preds_avg, on_step=True, on_epoch=True, prog_bar=False)
         self.log(
             "val/targets_avg",
             targets_avg,
             on_step=True,
             on_epoch=True,
-            prog_bar=True,
+            prog_bar=False,
         )
         return {
             "loss": loss,
@@ -133,31 +141,48 @@ class PointNetModel(LightningModule):
         }
 
     def on_validation_start(self):
+        """Create repository for validation clouds with predictions."""
         log_path = os.getcwd()
         self.val_preds_folder = osp.join(log_path, "validation_preds")
         os.makedirs(self.val_preds_folder, exist_ok=True)
+        self.val_preds_geotiffs_folder = osp.join(self.val_preds_folder, "geotiffs")
+        os.makedirs(self.val_preds_geotiffs_folder, exist_ok=True)
+
+    def save_val_las(self):
+        """After inference of classification in self.val_las, save:
+        - The LAS with updated classification
+        - A GeoTIFF of the classification, which is logged into Comet as well.
+        """
+        output_path = osp.join(
+            self.val_preds_folder,
+            f"{self.in_memory_tile_id}.las",
+        )
+        self.val_las.write(output_path)
+        log.info(f"Saved predictions to {output_path}")
+
+        # Create the tiff to self.val_preds_geotiffs_folder + name
+        output_path = osp.join(
+            self.val_preds_geotiffs_folder,
+            f"{self.in_memory_tile_id}.tif",
+        )
 
     def validation_step_end(self, output: dict):
         """Save the predicted classes in las format with position."""
-        # see https://laspy.readthedocs.io/en/latest/complete_tutorial.html
+
         if self.save_predictions:
             proba = output["proba"]
             preds = output["preds"]
             batch = output["batch"]
+            targets = output["targets"]
             for sample_idx in range(len(np.unique(batch.batch))):
                 elem_tile_id = batch.tile_id[sample_idx]
                 if self.in_memory_tile_id != elem_tile_id:
                     if self.in_memory_tile_id:
-                        output_path = osp.join(
-                            self.val_preds_folder,
-                            f"{self.in_memory_tile_id}.las",
-                        )
-                        self.val_las.write(output_path)
-                        log.info(f"Saved predictions to {output_path}")
+                        self.save_val_las()
 
-                    elem_filepath = batch.filepath[sample_idx]
                     self.in_memory_tile_id = elem_tile_id
-                    self.val_las = laspy.read(elem_filepath)
+
+                    self.val_las = laspy.read(batch.filepath[sample_idx])
                     # param = laspy.ExtraBytesParams(name="building_proba", type=float)
                     # self.val_las.add_extra_dim(param)
                     # TODO: consider setting this to np.nan or equivalent to capture incomplete predictions.
@@ -174,6 +199,9 @@ class PointNetModel(LightningModule):
                     self.val_las_pos = torch.from_numpy(self.val_las_pos)
 
                 elem_preds = preds[batch.batch == sample_idx]
+                elem_targets = targets[batch.batch == sample_idx]
+                if self.confusion_mode:
+                    elem_preds = elem_preds + 2 * elem_targets
                 elem_proba = proba[batch.batch == sample_idx][:, 1]
                 elem_pos = batch.origin_pos[batch.batch == sample_idx]
                 assign_idx = knn(self.val_las_pos, elem_pos, k=1, num_workers=1)[1]
