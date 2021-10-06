31e1fb6a4c0a9d84a5cb064df6f0e2ced22ced34
CharlesGaydon <charles.gaydon@gmail.com>
2021-10-06 11:44:57 +0200

    Updatre requirements and conda config

--- bash/setup_environment/setup_env.sh
+++ bash/setup_environment/setup_env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 conda install mamba -n base -c conda-forge
-mamba env create -f bash/setup_environment/requirements.yml
+# some mamba specific issue occurs as in https://github.com/conda-incubator/conda-lock/issues/101
+# mamba env create -f bash/setup_environment/requirements.yml  
+conda env create -f bash/setup_environment/requirements.yml
 conda activate validation_module
 
-pip install torch==1.8.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html  # if pytorch extensions are needed, use this torch install
-mamba install pyg==2.0.1==py39_torch_1.8.0_cu111 -c pyg -c conda-forge
-pip install torchvision==0.9.0
-pip install pytorch-lightning==1.3
+export CUDA="111"
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
+pip install torch-geometric
