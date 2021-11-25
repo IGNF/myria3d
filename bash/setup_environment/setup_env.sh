4ab52aded3f4bc94ab6f16ac915e2e41c23d2f6a
Michel Daab <michel.daab@ign.fr>
2021-11-25 11:35:06 +0100

    Improve the installation of the environment

--- bash/setup_environment/setup_env.sh
+++ bash/setup_environment/setup_env.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+# export PYTORCHVERSION="1.8.0"
+# export TORCHVISIONVERSION="0.9.0"
+
+export PYTORCHVERSION="1.8.1"
+export TORCHVISIONVERSION="0.9.1"
+# export CUDA="cu111"
+
 set -e
 conda install mamba -n base -c conda-forge
+
 # some mamba specific issue occurs as in https://github.com/conda-incubator/conda-lock/issues/101
 # mamba env create -f bash/setup_environment/requirements.yml
 mamba env create -f bash/setup_environment/requirements.yml
-conda activate validation_module_gpu
 
-conda install -y pytorch==1.8.0 torchvision cudatoolkit=11.1 -c pytorch -c nvidia
-mamba install -y pytorch-lightning==1.3 -c conda-forge 
-export CUDA="cu111"
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
-pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
-pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
-pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.8.0+${CUDA}.html
-pip install torch-geometric
+# eval "$(conda shell.bash hook)"
+conda activate validation_module
+
+# conda install -y pytorch==$PYTORCHVERSION torchvision==$TORCHVISIONVERSION -c pytorch
+pip3 install torch==$PYTORCHVERSION torchvision==$TORCHVISIONVERSION -f https://download.pytorch.org/whl/torch_stable.html
+mamba install -y pytorch-lightning==1.3 -c conda-forge
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-$PYTORCHVERSION+$CUDA.html
+conda install -y pytorch pyg -c pytorch -c pyg -c conda-forge
+
