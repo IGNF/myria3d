6ae478913d37a1cdfd29f76a042702c05d0c1a3a
CharlesGaydon <charles.gaydon@gmail.com>
2021-12-22 13:11:36 +0100

    Edit data preparation script

--- bash/data_preparation/split_clouds.sh
+++ bash/data_preparation/split_clouds.sh
@@ -1,10 +1,11 @@
 PIPELINE="/home/cgaydon/Segmentation-Validation-Model/bash/data_preparation/pipeline.json"
 LASFILES_DIR="/var/data/cgaydon/data/202110_building_val/trainvaltest/"
+mkdir ${LASFILES_DIR}split/
 for f in $(ls ${LASFILES_DIR}*.las);
 do
 	echo $f ;
 	BASENAME=$(basename $f .las);
 	echo $BASENAME;
-	mkdir ${SPLITTED_DIR}/${BASENAME}/
+	mkdir ${LASFILES_DIR}split/${BASENAME}/;
 	pdal translate --readers.las.override_srs="EPSG:2154" --readers.las.nosrs=true --writers.las.forward="all" --writers.las.extra_dims="all" --json $PIPELINE $f ${LASFILES_DIR}/split/${BASENAME}/${BASENAME}_SUB#.las ;
 done;
