c5a41e6fc1e6f8f77d189450cb8cd4dc5a76f77c
CharlesGaydon <charles.gaydon@gmail.com>
2022-03-23 16:42:13 +0100

    Use pdal instead of laspy for VLRs-robust interpolation

--- bash/setup_environment/setup_env.sh
+++ bash/setup_environment/setup_env.sh
@@ -3,7 +3,7 @@ set -e
 # SETUP
 conda install -y mamba -n base -c conda-forge
 mamba env create -f bash/setup_environment/requirements.yml
-conda activate lidar_deep_segmentation
+conda activate lidar_multiclass
 
 # INSTALL
 export PYTORCHVERSION="1.10.1"
@@ -13,4 +13,4 @@ mamba install -y pytorch==$PYTORCHVERSION torchvision==$TORCHVISIONVERSION cudat
 mamba install -y pyg==2.0.3 -c pytorch -c pyg -c conda-forge
 FORCE_CUDA=1 pip install torch-points-kernels --no-cache
 pip install numba==0.55.1 numpy==1.20.0 # revert inconsistent torch-points-kernel dependencies
-mamba install pytorch-lightning==1.5.9 -c conda-forge
+mamba install -y pytorch-lightning==1.5.9 -c conda-forge
