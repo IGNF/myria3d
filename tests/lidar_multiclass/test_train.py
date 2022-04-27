d382536feb891fc3e5cc766b575f51b91de28771
Charles GAYDON <charles.gaydon@gmail.com>
2022-04-27 16:27:18 +0200

    Test training for both RandLaNet and PointNet on an overfit task

--- tests/lidar_multiclass/test_train.py
+++ tests/lidar_multiclass/test_train.py
@@ -33,10 +33,8 @@ def test_RandLaNet_overfitting(make_default_hydra_cfg, isolated_toy_dataset_tmpd
         cfg = make_default_hydra_cfg(
             overrides=[
                 "experiment=RandLaNetDebug",  # Use an experiment designe for overfitting a batch
-                # "model=point_net_model",  # but use PointNet model instead of RandLaNet
                 "logger=csv",  # disables comet logging
                 "datamodule.batch_size=2",  # Smaller batch size for faster overfit
-                "datamodule.num_workers=2",  # More workers to avoid bottleneck
                 f"datamodule.prepared_data_dir={isolated_toy_dataset_tmpdir}",
                 f"logger.csv.save_dir={tmpdir}",
             ]
@@ -47,3 +45,31 @@ def test_RandLaNet_overfitting(make_default_hydra_cfg, isolated_toy_dataset_tmpd
         iou = metrics["train/iou_CLASS_building"].dropna()
         improvement = iou.iloc[-1] - iou.iloc[0]
         assert improvement >= 0.75
+
+
+@pytest.mark.slow()
+def test_PointNet_overfitting(make_default_hydra_cfg, isolated_toy_dataset_tmpdir):
+    with TemporaryDirectory() as tmpdir:
+        cfg = make_default_hydra_cfg(
+            overrides=[
+                "experiment=RandLaNetDebug",  # Use an experiment designe for overfitting a batch
+                "model=point_net_model",  # but change model to PointNet
+                "model.lr=0.001",  # need higher LR for PointNet
+                "logger=csv",  # disables comet logging
+                "datamodule.batch_size=2",  # Smaller batch size for faster overfit
+                # Define the task as a classification of all (1 and 2) vs. 6=building
+                "datamodule.dataset_description.classification_preprocessing_dict={2:1}",
+                f"datamodule.prepared_data_dir={isolated_toy_dataset_tmpdir}",
+                f"logger.csv.save_dir={tmpdir}",
+            ]
+        )
+        train(cfg)
+        # Not sure if version_0 is added by pytest or by lightning, but it is needed.
+        metrics = pd.read_csv(osp.join(tmpdir, "csv", "version_0", "metrics.csv"))
+        iou = metrics["train/loss_step"].dropna()
+        first = iou.iloc[0]
+        last = iou.iloc[-1]
+        # Assert that loss was almost divided by two
+        assert pytest.approx(last, 0.15) == first / 2
+        # Note that for this toy dataset PointNet gets a null IoU for building class, which may be due
+        # to an inability to catch buildings, which are a rare class
