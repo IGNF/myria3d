68ed1575cdebb1a967a7e0f4adaeaa122483deba
CharlesGaydon <charles.gaydon@gmail.com>
2022-07-07 10:51:39 +0200

    Fix edge case batch_size=1 and implement related test.

--- tests/myria3d/models/modules/test_randla_net.py
+++ tests/myria3d/models/modules/test_randla_net.py
@@ -1,10 +1,17 @@
+import pytest
 import torch
 from torch_geometric.data import Batch
 from myria3d.models.modules.randla_net import RandLANet
 
 
-def test_fake_run_randlanet():
-    """Documents expected data format and make a forward pass with RandLa-Net"""
+@pytest.mark.parametrize("num_graphs", [1, 4])
+def test_fake_run_randlanet(num_graphs):
+    """Documents expected data format and make a forward pass with RandLa-Net
+
+    Model pass with "batch_size=1" is a edge case that needs to pass to avoid unexpected crash due to incomplete batch at the
+    end of an inference.
+
+    """
     num_euclidian_dimensions = 3
     num_features = 9
     d_in = num_euclidian_dimensions + num_features
@@ -17,7 +24,7 @@ def test_fake_run_randlanet():
         "num_classes": num_classes,
     }
     batch = Batch()
-    batch.num_graphs = 4
+    batch.num_graphs = 1
     num_points = 12500
     batch.pos = torch.rand((num_points * batch.num_graphs, num_euclidian_dimensions))
     batch.x = torch.rand((num_points * batch.num_graphs, num_features))
