54a3ea05ba7011e31d4e6d11ffbe8eae07b50443
CharlesGaydon <charles.gaydon@gmail.com>
2022-09-22 15:06:59 +0200

    Refactor HDF dataset for clarity and docstrings.

--- myria3d/pctl/dataset/hdf5.py
+++ myria3d/pctl/dataset/hdf5.py
@@ -61,7 +61,15 @@ class HDF5Dataset(Dataset):
         self.train_transform = train_transform
         self.eval_transform = eval_transform
 
+        self.tile_width = tile_width
+        self.subtile_width = subtile_width
+        self.subtile_overlap_train = subtile_overlap_train
+        self.subtile_shape = subtile_shape
+
         self.hdf5_file_path = hdf5_file_path
+
+        # Instantiates these to null;
+        # They are loaded within __getitem__ to support multi-processing training.
         self.dataset = None
         self.samples_hdf5_paths = None
 
@@ -71,74 +79,87 @@ class HDF5Dataset(Dataset):
             )
             return
 
-        # CONVERSION TO a SINGLE H5 file
+        # Add data for all LAS Files into a single hdf5 file.
         os.makedirs(osp.dirname(hdf5_file_path), exist_ok=True)
-        for s, raw_paths in las_files_by_split.items():
+        for split, las_paths in las_files_by_split.items():
             with h5py.File(hdf5_file_path, "a") as f:
-                if s not in f:
-                    f.create_group(s)
-            for las_path in tqdm(raw_paths, desc=f"Preparing {s} set..."):
-                b = osp.basename(las_path)
-                # Delete incomplete file entry to start from scratch.
-                with h5py.File(hdf5_file_path, "a") as f:
-                    if b in f[s] and "is_complete" not in f[s][b].attrs:
-                        del f[b]
+                if split not in f:
+                    f.create_group(split)
+            for las_path in tqdm(las_paths, desc=f"Preparing {split} set..."):
+                self._add_to_dataset(split, las_path)
+
+        self._index_all_samples_as_needed()
+
+    def _add_to_dataset(self, split: str, las_path: str):
+        """Add samples from LAS into HDF5 dataset file.
+
+        Args:
+            split (str): specifies either "train", "val", or "test" split.
+            las_path (str): path to point cloud.
+
+        """
+        b = osp.basename(las_path)
+
+        # Delete dataset for incomplete LAS entry, to start from scratch.
+        # Useful in case data preparation was interrupted.
+        with h5py.File(self.hdf5_file_path, "a") as f:
+            if b in f[split] and "is_complete" not in f[split][b].attrs:
+                del f[b]
                 # Parse and add subtiles to split group.
-                with h5py.File(hdf5_file_path, "a") as f:
-                    if b not in f[s]:
-                        sample_number = 0
-                        for sample_idx, sample_points in split_cloud_into_samples(
-                            las_path,
-                            tile_width,
-                            subtile_width,
-                            subtile_shape,
-                            subtile_overlap=subtile_overlap_train
-                            if s == "train"
-                            else 0,
-                        ):
-                            data = self.points_pre_transform(sample_points)
-                            if self.pre_filter is not None and self.pre_filter(data):
-                                # e.g. not enough points in this receptive field.
-                                continue
-                            sample_id = str(sample_number).zfill(5)
-                            hdf5_path = osp.join(s, b, sample_id)
-                            hd5f_path_x = osp.join(hdf5_path, "x")
-                            f.create_dataset(
-                                hd5f_path_x,
-                                data.x.shape,
-                                dtype="f",
-                                data=data.x,
-                            )
-                            f[hd5f_path_x].attrs["x_features_names"] = copy.deepcopy(
-                                data.x_features_names
-                            )
-                            f.create_dataset(
-                                osp.join(hdf5_path, "pos"),
-                                data.pos.shape,
-                                dtype="f",
-                                data=data.pos,
-                            )
-                            f.create_dataset(
-                                osp.join(hdf5_path, "y"),
-                                data.y.shape,
-                                dtype="i",
-                                data=data.y,
-                            )
-                            f.create_dataset(
-                                osp.join(hdf5_path, "idx_in_original_cloud"),
-                                sample_idx.shape,
-                                dtype="i",
-                                data=sample_idx,
-                            )
-                            sample_number += 1
+        with h5py.File(self.hdf5_file_path, "a") as f:
+            if b not in f[split]:
+                sample_number = 0
+                for sample_idx, sample_points in split_cloud_into_samples(
+                    las_path,
+                    self.tile_width,
+                    self.subtile_width,
+                    self.subtile_shape,
+                    # No overlap at eval time.
+                    subtile_overlap=self.subtile_overlap_train
+                    if split == "train"
+                    else 0,
+                ):
+                    data = self.points_pre_transform(sample_points)
+                    if self.pre_filter is not None and self.pre_filter(data):
+                        # e.g. pre_filter spots situations where num_nodes is too small.
+                        continue
+                    sample_id = str(sample_number).zfill(5)
+                    hdf5_path = osp.join(split, b, sample_id)
+                    hd5f_path_x = osp.join(hdf5_path, "x")
+                    f.create_dataset(
+                        hd5f_path_x,
+                        data.x.shape,
+                        dtype="f",
+                        data=data.x,
+                    )
+                    f[hd5f_path_x].attrs["x_features_names"] = copy.deepcopy(
+                        data.x_features_names
+                    )
+                    f.create_dataset(
+                        osp.join(hdf5_path, "pos"),
+                        data.pos.shape,
+                        dtype="f",
+                        data=data.pos,
+                    )
+                    f.create_dataset(
+                        osp.join(hdf5_path, "y"),
+                        data.y.shape,
+                        dtype="i",
+                        data=data.y,
+                    )
+                    f.create_dataset(
+                        osp.join(hdf5_path, "idx_in_original_cloud"),
+                        sample_idx.shape,
+                        dtype="i",
+                        data=sample_idx,
+                    )
+                    sample_number += 1
 
-                        # A termination flag to report that all samples for this point cloud were included in the df5 file.
-                        f[s][b].attrs["is_complete"] = True
+                # A termination flag to report that all samples for this point cloud were included in the df5 file.
+                f[split][b].attrs["is_complete"] = True
 
-        self.index_all_samples_as_needed()
-
-    def index_all_samples_as_needed(self):
-        """Index all samples in the dataset."""
+    def _index_all_samples_as_needed(self):
+        """Index all samples in the dataset, if not already done before."""
         # Use existing if already indexed. Need to decode b-string
         if self.samples_hdf5_paths is not None:
             return
@@ -174,7 +195,7 @@ class HDF5Dataset(Dataset):
     def __len__(self):
         return len(self.samples_hdf5_paths)
 
-    def _get_data(self, sample_hdf5_path) -> Data:
+    def _get_data(self, sample_hdf5_path: str) -> Data:
         """Loads a Data object from the HDF5 dataset.
 
         Opening the file has a high cost so we do it only once and store the opened files as a singleton
@@ -184,8 +205,6 @@ class HDF5Dataset(Dataset):
         """
         if self.dataset is None:
             self.dataset = h5py.File(self.hdf5_file_path, "r")
-
-        self.index_all_samples_as_needed()
 
         grp = self.dataset[sample_hdf5_path]
         # [...] needed to make a copy of content and avoid closing HDF5.
@@ -200,7 +219,8 @@ class HDF5Dataset(Dataset):
             x_features_names=grp["x"].attrs["x_features_names"].tolist(),
         )
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Optional[Data]:
+        self._index_all_samples_as_needed()
         sample_hdf5_path = self.samples_hdf5_paths[idx]
         data = self._get_data(sample_hdf5_path)
 
@@ -210,13 +230,17 @@ class HDF5Dataset(Dataset):
 
         # Transforms, including sampling and some augmentations.
         transform = self.train_transform
-        if sample_hdf5_path.startswith("val") or sample_hdf5_path.startswith("test"):
+        if sample_hdf5_path.startswith("val") or sample_hdf5_path.startswith(
+            "test"
+        ):
             transform = self.eval_transform
         if transform is not None:
             data = transform(data)
 
         # filter if empty
-        if data is None or (self.pre_filter is not None and self.pre_filter(data)):
+        if data is None or (
+            self.pre_filter is not None and self.pre_filter(data)
+        ):
             return None
 
         return data
@@ -234,8 +258,11 @@ class HDF5Dataset(Dataset):
         return self._get_split_subset("test")
 
     def _get_split_subset(self, split: SPLIT_TYPE):
-        self.index_all_samples_as_needed()
+        """Get a sub-dataset of a specific (train/val/test) split."""
+        self._index_all_samples_as_needed()
         indices = [
-            idx for idx, p in enumerate(self.samples_hdf5_paths) if p.startswith(split)
+            idx
+            for idx, p in enumerate(self.samples_hdf5_paths)
+            if p.startswith(split)
         ]
         return torch.utils.data.Subset(self, indices)
