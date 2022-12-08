86e038aea3d8e3427d0f061fc62ec88368a8c6be
CharlesGaydon <charles.gaydon@gmail.com>
2022-12-08 16:53:36 +0100

    V3.2.0: control of output channels names (PredictedClassification and Entropy)

--- myria3d/models/interpolation.py
+++ myria3d/models/interpolation.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from enum import Enum
-from typing import Dict, List, Literal, Union
+from typing import Dict, List, Literal, Optional, Union
 
 import numpy as np
 import pdal
@@ -14,13 +14,6 @@ from myria3d.pctl.dataset.utils import get_pdal_reader
 log = logging.getLogger(__name__)
 
 
-class ChannelNames(Enum):
-    """Names of custom additional LAS channel."""
-
-    PredictedClassification = "PredictedClassification"
-    ProbasEntropy = "entropy"
-
-
 @torch.no_grad()
 class Interpolator:
     """A class to load, update with classification, update with probas (optionnal), and save a LAS."""
@@ -30,6 +23,8 @@ class Interpolator:
         interpolation_k: int = 10,
         classification_dict: Dict[int, str] = {},
         probas_to_save: Union[List[str], Literal["all"]] = "all",
+        predicted_classification_channel: Optional[str] = "PredictedClassification",
+        entropy_channel: Optional[str] = "entropy",
     ):
         """Initialization method.
         Args:
@@ -43,6 +38,8 @@ class Interpolator:
 
         self.k = interpolation_k
         self.classification_dict = classification_dict
+        self.predicted_classification_channel = predicted_classification_channel
+        self.entropy_channel = entropy_channel
 
         if probas_to_save == "all":
             self.probas_to_save = list(classification_dict.values())
@@ -57,7 +54,7 @@ class Interpolator:
         self.logits: List[torch.Tensor] = []
         self.idx_in_full_cloud_list: List[np.ndarray] = []
 
-    def load_full_las_for_update(self, src_las: str):
+    def load_full_las_for_update(self, src_las: str) -> np.ndarray:
         """Loads a LAS and adds necessary extradim.
 
         Args:
@@ -65,22 +62,30 @@ class Interpolator:
         """
         # self.current_f = filepath
         pipeline = get_pdal_reader(src_las)
-        new_dims = self.probas_to_save + [
-            ChannelNames.PredictedClassification.value,
-            ChannelNames.ProbasEntropy.value,
-        ]
-        for new_dim in new_dims:
-            pipeline |= pdal.Filter.ferry(dimensions=f"=>{new_dim}") | pdal.Filter.assign(value=f"{new_dim}=0")
-        pipeline.execute()
-        return pipeline.arrays[0]  # named array
+        for proba_channel_to_create in self.probas_to_save:
+            pipeline |= pdal.Filter.ferry(dimensions=f"=>{proba_channel_to_create}")
+            pipeline |= pdal.Filter.assign(value=f"{proba_channel_to_create}=0")
 
-    def store_predictions(self, logits, idx_in_original_cloud):
+        if self.predicted_classification_channel:
+            # Copy from Classification to preserve data type.
+            if self.predicted_classification_channel != "Classification":
+                pipeline |= pdal.Filter.ferry(dimensions=f"Classification=>{self.predicted_classification_channel}")
+            # Reset channel.
+            pipeline |= pdal.Filter.assign(value=f"{self.predicted_classification_channel}=0")
+
+        if self.entropy_channel:
+            pipeline |= pdal.Filter.ferry(dimensions=f"=>{self.entropy_channel}") | pdal.Filter.assign(value=f"{self.entropy_channel}=0")
+
+        pipeline.execute()
+        return pipeline.arrays[0]
+
+    def store_predictions(self, logits, idx_in_original_cloud) -> None:
         """Keep a list of predictions made so far."""
         self.logits += [logits]
         self.idx_in_full_cloud_list += idx_in_original_cloud
 
     @torch.no_grad()
-    def reduce_predicted_logits(self, las):
+    def reduce_predicted_logits(self, las) -> torch.Tensor:
         """Interpolate logits to points without predictions using an inverse-distance weightning scheme.
 
         Returns:
@@ -124,16 +129,25 @@ class Interpolator:
             if class_name in self.probas_to_save:
                 las[class_name] = probas[:, idx]
 
-        preds = torch.argmax(logits, dim=1)
-        preds = np.vectorize(self.reverse_mapper.get)(preds)
-        las[ChannelNames.PredictedClassification.value] = preds
+        if self.predicted_classification_channel:
+            preds = torch.argmax(logits, dim=1)
+            preds = np.vectorize(self.reverse_mapper.get)(preds)
+            las[self.predicted_classification_channel] = preds
+            log.info(
+                f"Saving predicted classes to channel {self.predicted_classification_channel}."
+                "Channel name can be changed by setting `predict.interpolator.predicted_classification_channel`."
+            )
 
-        las[ChannelNames.ProbasEntropy.value] = Categorical(probs=probas).entropy()
-
+        if self.entropy_channel:
+            las[self.entropy_channel] = Categorical(probs=probas).entropy()
+            log.info(
+                f"Saving Shannon entropy of probabilities to channel {self.entropy_channel}."
+                "Channel name can be changed by setting `predict.interpolator.entropy_channel`"
+            )
         os.makedirs(output_dir, exist_ok=True)
         out_f = os.path.join(output_dir, basename)
         out_f = os.path.abspath(out_f)
-        log.info(f"Updated LAS ({basename}) will be saved to \n {output_dir}\n")
+        log.info(f"Updated LAS ({basename}) will be saved to: \n {output_dir}\n")
         log.info("Saving...")
         pipeline = pdal.Writer.las(filename=out_f, extra_dims="all", minor_version=4, dataformat_id=8).pipeline(las)
         pipeline.execute()
