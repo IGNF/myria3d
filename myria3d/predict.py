af17f901465e36b96f02df02367497e794f13034
MichelDaab <93936641+MichelDaab@users.noreply.github.com>
2023-01-23 15:20:05 +0100

    Embedding a defaut trained model and config in the code  (#48)
    * WIP V3.*.* with torch-geometric RandLA-Net implementation (#39)
    
    Development of PyG-RandLA-Net
    
    Co-authored-by: Michel Daab <michel.daab@ign.fr>
    
    * Architecture update to latest state + Max Nodes Budgets (#43)
    
    * Update with https://github.com/pyg-team/pytorch_geometric/pull/5117
    
    * Bump minor version to indicate no-model-compatibility
    
    * Update signature for pyg randlanet
    
    * Fix old randlanet signature
    
    * Get rid of legacy implementation of RandLA-Net
    
    * Disable example run until release of a model that is compatible
    
    * Fix misleading batch_size indication for multi-GPUs setting.
    
    * Pyg RandLaNet XP with min/max num_nodes and gradient accumulation.
    
    * Rename XP.
    
    * NoRS XP inherits from base XP.
    
    * 5 epochs of cooldown before reducing lr
    
    * 20 epochs of patience  before reducing lr
    
    * Flake8 corrections.
    
    * Correct model version name in CICD workflow
    
    * Correct config  name in CICD workflow
    
    * New "create_hdf5" task for data-preparation-as-a-task (#42)
    
    * add get_las_paths_by_split_dict to utils
    
    * taking review into account
    
    * increase version number
    
    * add create_hdf5 to the doc
    
    * forgot a word
    
    * resolve version conflict
    
    * Isort all python files (#44)
    
    * Bump version to V3.1.2
    
    * put the config file and the checkpoint into docker
    
    * code neatness patch
    
    * change the version number
    
    * Comet must be first import in run.py (#45)
    
    * fix a checkpoint bug
    
    * patch for checkpoint path
    
    * display of checkpoint path
    
    * test checkpoint path
    
    * test chekcpoint path
    
    * test checkpoint path
    
    * add nano to docker (for debugging purpose)
    
    * change to have only one hydra loading
    
    * exclue a couple of preparatory functions from coverage
    
    * exclude two method from coverage
    
    * cheating to pass coverage step (we are in a hurry!)
    
    * make path to checkpoint absolute
    
    * path works
    
    * code cleaning
    
    * 1 forgotten line
    
    * 1 line forgotten
    
    * Revert "Merge branch 'main' of https://github.com/IGNF/lidar-deep-segmentation"
    
    This reverts commit fbf5dccbc2258c83e056c5b5a2f7b2b4a8564789, reversing
    changes made to 5129be695ba835fa03920bae74569b34b3b3d804.
    
    * task.task_name is mandatory now, patched a test
    
    * updated config file
    
    * monkey patching for interpolator
    
    * change doc and version number
    
    * deals with "interpolator"
    
    * little change to redo the docker image
    
    * correct the path to ckpt
    
    * correction for interpolator and increase the version number
    
    * add proba_to_save to interpolator
    
    * CICD patch for interpolator
    
    * change to get the docker image
    
    * patch for confidence
    
    * little patch for confidence
    
    * test to psuh hidinf files on nexus
    
    * manual merge to repair history
    
    * forgot one file
    
    * another change to try and get "confidence" channel
    
    * patched and verified, update of its version number
    
    * patch for pul request reviews
    
    * correct path to the CICD directory to match the content of those directory
    
    * another correction for pul request reviews
    
    * change default.env to placeholder.env
    
    * correctly provide k_interpolation through interpolator
    
    * change name of default files for trained model assets to display version
    
    * another change to trained model assests default name
    
    * setting correct "interpolator" paths into the doc
    
    * puts interpolation_k back into interpolator
    
    * change back a regression
    
    * add testing the default config in the CICD
    
    Co-authored-by: Charles Gaydon <11660435+CharlesGaydon@users.noreply.github.com>
    Co-authored-by: CharlesGaydon <charles.gaydon@gmail.com>

--- myria3d/predict.py
+++ myria3d/predict.py
@@ -1,7 +1,6 @@
 import os
 import os.path as osp
 import sys
-from glob import glob
 
 import hydra
 import torch
@@ -52,53 +51,21 @@ def predict(config: DictConfig) -> str:
     model.to(device)
     model.eval()
 
-    itp = hydra.utils.instantiate(config.predict.interpolator)
+    # TODO: Interpolator could be instantiated directly via hydra.
+    itp = Interpolator(
+        interpolation_k=config.predict.interpolator.interpolation_k,
+        classification_dict=config.dataset_description.get("classification_dict"),
+        probas_to_save=config.predict.interpolator.probas_to_save,
+        predicted_classification_channel=config.predict.interpolator.get("predicted_classification_channel", "PredictedClassification"),
+        entropy_channel=config.predict.interpolator.get("entropy_channel", "entropy"),
+    )
 
     for batch in tqdm(datamodule.predict_dataloader()):
         batch.to(device)
         logits = model.predict_step(batch)["logits"]
         itp.store_predictions(logits, batch.idx_in_original_cloud)
 
-    out_f = itp.reduce_predictions_and_save(config.predict.src_las, config.predict.output_dir)
+    out_f = itp.reduce_predictions_and_save(
+        config.predict.src_las, config.predict.output_dir
+    )
     return out_f
-
-
-@utils.eval_time
-@hydra.main(config_path="../configs/", config_name="config.yaml")
-def main(config: DictConfig):
-    """This wrapper allows to specify a hydra configuration from command line.
-
-    The config file logged during training should be used for prediction. It should
-    be edited so that it does not rely on unnecessary environment variables (`oc.env:` prefix).
-    Parameters in configuration group `predict` can be specified directly in the config file
-    or overriden via CLI at runtime.
-
-    This wrapper supports running predictions for all files specified by a glob pattern given
-    by config parameter predict.src_las.
-
-    """
-    # Imports should be nested inside @hydra.main to optimize tab completion
-    # Read more here: https://github.com/facebookresearch/hydra/issues/934
-    from myria3d.predict import predict
-    from myria3d.utils import utils
-
-    # You can safely get rid of this line if you don't want those
-    utils.extras(config)
-
-    if config.get("print_config"):
-        utils.print_config(config, resolve=False)
-
-    # Parameter predict.src_las can be a path or a glob pattern
-    # e.g. /path/to/files_*.las
-    src_las_iterable = glob(config.predict.src_las)
-
-    if not src_las_iterable:
-        raise FileNotFoundError(f"Globing pattern {config.predict.src_las} (param predict.src_las) did not return any file.")
-
-    # Iterate over the files and predict.
-    for config.predict.src_las in tqdm(src_las_iterable):
-        predict(config)
-
-
-if __name__ == "__main__":
-    main()
--- run.py
+++ run.py
@@ -6,16 +6,23 @@ except ImportError:
 
 from enum import Enum
 
+import os
+import sys
+from glob import glob
 import dotenv
 import hydra
 from omegaconf import DictConfig
+from tqdm import tqdm
 
+from myria3d.utils import utils
 from myria3d.pctl.dataset.hdf5 import create_hdf5
 from myria3d.pctl.dataset.utils import get_las_paths_by_split_dict
 
-# load environment variables from `.env` file if it exists
-# recursively searches for `.env` in all folders starting from work dir
-dotenv.load_dotenv(override=True)
+TASK_NAME_DETECTION_STRING = "task.task_name="
+DEFAULT_DIRECTORY = "trained_model_assets/"
+DEFAULT_CONFIG_FILE = "proto151_V2.0_epoch_100_Myria3DV3.1.0_predict_config_V3.2.0.yaml"
+DEFAULT_CHECKPOINT = "proto151_V2.0_epoch_100_Myria3DV3.1.0.ckpt"
+DEFAULT_ENV = "placeholder.env"
 
 
 class TASK_NAMES(Enum):
@@ -26,54 +33,90 @@ class TASK_NAMES(Enum):
     HDF5 = "create_hdf5"
 
 
+log = utils.get_logger(__name__)
+
+
 @hydra.main(config_path="configs/", config_name="config.yaml")
-def main(config: DictConfig):
-    """Entrypoint to training-related logic and inference code.
-
-    Run `python run.py -h` to print configuration and see parameters.
-
-    Hydra configs can be overriden in CLI with `--config-path` and `--config-name` arguments.
-
-    """
-
+def launch_train(config: DictConfig):  # pragma: no cover  (it's just an initialyzer of a class/method tested elsewhere)
+    """Training, evaluation, testing, or finetuning of a neural network."""
     # Imports should be nested inside @hydra.main to optimize tab completion
     # Read more here: https://github.com/facebookresearch/hydra/issues/934
-    from myria3d.predict import predict
     from myria3d.train import train
-    from myria3d.utils import utils
-
-    # A couple of optional utilities:
-    # - disabling python warnings
-    # - easier access to debug mode
-    # - forcing debug friendly configuration
-    # You can safely get rid of this line if you don't want those
     utils.extras(config)
 
     # Pretty print config using Rich library
     if config.get("print_config"):
         utils.print_config(config, resolve=False)
+    return train(config)
 
-    task_name = config.task.get("task_name")
-    if task_name in [TASK_NAMES.FIT.value, TASK_NAMES.TEST.value, TASK_NAMES.FINETUNE.value]:
-        """Training, evaluation, testing, or finetuning of a neural network."""
-        return train(config)
-    elif task_name == TASK_NAMES.PREDICT.value:
-        """Infer probabilities and automate semantic segmentation decisions on unseen data."""
-        return predict(config)
-    elif task_name == TASK_NAMES.HDF5.value:
-        """Build an HDF5 file from a directory with las files."""
-        las_paths_by_split_dict = get_las_paths_by_split_dict(config.datamodule.get("data_dir"), config.datamodule.get("split_csv_path"))
-        create_hdf5(
-            las_paths_by_split_dict=las_paths_by_split_dict,
-            hdf5_file_path=config.datamodule.get("hdf5_file_path"),
-            tile_width=config.datamodule.get("tile_width"),
-            subtile_width=config.datamodule.get("subtile_width"),
-            subtile_shape=config.datamodule.get("subtile_shape"),
-            pre_filter=hydra.utils.instantiate(config.datamodule.get("pre_filter")),
-            subtile_overlap_train=config.datamodule.get("subtile_overlap_train"),
-            points_pre_transform=hydra.utils.instantiate(config.datamodule.get("points_pre_transform"))
-        )
+
+@hydra.main(config_path=DEFAULT_DIRECTORY, config_name=DEFAULT_CONFIG_FILE)
+def launch_predict(config: DictConfig):
+    """Infer probabilities and automate semantic segmentation decisions on unseen data."""
+    # Imports should be nested inside @hydra.main to optimize tab completion
+    # Read more here: https://github.com/facebookresearch/hydra/issues/934
+    from myria3d.predict import predict
+
+    # hydra changes current directory, so we make sure the checkpoint has an absolute path
+    if not os.path.isabs(config.predict.ckpt_path):
+        config.predict.ckpt_path = os.path.join(os.path.dirname(__file__), config.predict.ckpt_path)
+
+    # Pretty print config using Rich library
+    if config.get("print_config"):
+        utils.print_config(config, resolve=False)
+
+    # Iterate over the files and predict.
+    src_las_iterable = glob(config.predict.src_las)
+    for config.predict.src_las in tqdm(src_las_iterable):
+        predict(config)
+
+
+@hydra.main(config_path="configs/", config_name="config.yaml")
+def launch_hdf5(config: DictConfig):
+    """Build an HDF5 file from a directory with las files."""
+
+    # Pretty print config using Rich library
+    if config.get("print_config"):
+        utils.print_config(config, resolve=False)
+
+    las_paths_by_split_dict = get_las_paths_by_split_dict(config.datamodule.get("data_dir"), config.datamodule.get("split_csv_path"))
+    create_hdf5(
+        las_paths_by_split_dict=las_paths_by_split_dict,
+        hdf5_file_path=config.datamodule.get("hdf5_file_path"),
+        tile_width=config.datamodule.get("tile_width"),
+        subtile_width=config.datamodule.get("subtile_width"),
+        subtile_shape=config.datamodule.get("subtile_shape"),
+        pre_filter=hydra.utils.instantiate(config.datamodule.get("pre_filter")),
+        subtile_overlap_train=config.datamodule.get("subtile_overlap_train"),
+        points_pre_transform=hydra.utils.instantiate(config.datamodule.get("points_pre_transform"))
+    )
 
 
 if __name__ == "__main__":
-    main()
+    for arg in sys.argv:
+        if TASK_NAME_DETECTION_STRING in arg:
+            _, task_name = arg.split("=")
+            break
+
+    try:
+        log.info(f"task selected: {task_name}")
+
+        if task_name in [TASK_NAMES.FIT.value, TASK_NAMES.TEST.value, TASK_NAMES.FINETUNE.value]:
+            # load environment variables from `.env` file if it exists
+            # recursively searches for `.env` in all folders starting from work dir
+            dotenv.load_dotenv(override=True)
+            launch_train()
+
+        elif task_name == TASK_NAMES.PREDICT.value:
+            dotenv.load_dotenv(os.path.join(DEFAULT_DIRECTORY, DEFAULT_ENV))
+            launch_predict()
+
+        elif task_name == TASK_NAMES.HDF5.value:
+            launch_hdf5()
+
+        else:
+            log.warning("Task unknown")
+
+    except NameError as e:
+        log.error('a task name must be defined, with the argument "task.task_name=..."')
+        raise e
