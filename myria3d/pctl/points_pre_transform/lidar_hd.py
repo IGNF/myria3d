07f27b632171de46641a54f200a38e4a36a63a36
Michel Daab <michel.daab@ign.fr>
2023-11-02 15:14:18 +0100

    Add geometry, scan angle and color

--- myria3d/pctl/points_pre_transform/lidar_hd.py
+++ myria3d/pctl/points_pre_transform/lidar_hd.py
@@ -1,6 +1,12 @@
 # function to turn points loaded via pdal into a pyg Data object, with additional channels
+from colorsys import rgb_to_hsv
+from math import pi, sin, cos
+
 import numpy as np
 from torch_geometric.data import Data
+
+from pgeof import pgeof
+from sklearn.neighbors import NearestNeighbors
 
 COLORS_NORMALIZATION_MAX_VALUE = 255.0 * 256.0
 RETURN_NUMBER_NORMALIZATION_MAX_VALUE = 7.0
@@ -33,6 +39,31 @@ def lidar_hd_pre_transform(points):
         points[color][:] = points[color] / COLORS_NORMALIZATION_MAX_VALUE
         points[color][occluded_points] = 0.0
 
+
+    x_features_names = [
+        "Intensity",
+        "ReturnNumber",
+        "NumberOfReturns",
+        "Red",
+        "Green",
+        "Blue",
+        "Cos_Hue",
+        "Sin_Hue",
+        "Shade",
+        "Value",
+        "Infrared",
+        "ScanAngleRank",
+        "rgb_avg",
+        "ndvi",
+        "linearity",
+        "planarity",
+        "scattering",
+        "verticality",
+        "normal_x",
+        "normal_y",
+        "normal_z",
+    ]
+
     # Additional features :
     # Average color, that will be normalized on the fly based on single-sample
     rgb_avg = (
@@ -44,34 +75,74 @@ def lidar_hd_pre_transform(points):
     # NDVI
     ndvi = (points["Infrared"] - points["Red"]) / (points["Infrared"] + points["Red"] + 10**-6)
 
-    # todo
-    x = np.stack(
-        [
-            points[name]
-            for name in [
-                "Intensity",
-                "ReturnNumber",
-                "NumberOfReturns",
-                "Red",
-                "Green",
-                "Blue",
-                "Infrared",
+    # geometry
+    k = min(100, len(points))   # in case there are too few points
+    kneigh = NearestNeighbors(n_neighbors=k).fit(pos).kneighbors(pos)
+    nn_ptr = np.arange(pos.shape[0] + 1) * k
+    nn = kneigh[1].flatten()
+
+    # Make sure xyz are float32 and nn and nn_ptr are uint32
+    pos = pos.astype('float32')
+    nn_ptr = nn_ptr.astype('uint32')
+    nn = nn.astype('uint32')
+
+    # Make sure arrays are contiguous (C-order) and not Fortran-order
+    pos = np.ascontiguousarray(pos)
+    nn_ptr = np.ascontiguousarray(nn_ptr)
+    nn = np.ascontiguousarray(nn)  
+
+    geof = pgeof(points, nn, nn_ptr, k_min=10, k_step=1, k_min_search=15, verbose=True).transpose()
+
+
+    # Pre-allocate memory
+    x = np.empty((points.shape[0], len(x_features_names)))
+
+    # Fill x
+    for index, point in enumerate(points):
+        hue, shade, value = rgb_to_hsv(point["Red"], point["Green"], point["Blue"])
+        x[index] = [
+            point["Intensity"],
+            point["ReturnNumber"],
+            point["NumberOfReturns"],
+            point["Red"],
+            point["Green"],
+            point["Blue"],          
+            cos(2 * pi * hue),
+            sin(2 * pi * hue),
+            shade,
+            value,
+            point["Infrared"],
+            point["ScanAngleRank"],
+            rgb_avg[index],
+            ndvi[index],
+            geof[0][index],    # linearity
+            geof[1][index],    # planarity
+            geof[2][index],    # scattering
+            geof[3][index],    # verticality
+            geof[4][index],    # normal_x
+            geof[5][index],    # normal_y
+            geof[6][index],    # normal_z
             ]
-        ]
-        + [rgb_avg, ndvi],
-        axis=0,
-    ).transpose()
-    x_features_names = [
-        "Intensity",
-        "ReturnNumber",
-        "NumberOfReturns",
-        "Red",
-        "Green",
-        "Blue",
-        "Infrared",
-        "rgb_avg",
-        "ndvi",
-    ]
+
+    # # todo
+    # x = np.stack(
+    #     [
+    #         points[name]
+    #         for name in [
+    #             "Intensity",
+    #             "ReturnNumber",
+    #             "NumberOfReturns",
+    #             "Red",
+    #             "Green",
+    #             "Blue",
+    #             "Infrared",
+    #         ]
+    #     ]
+    #     + [rgb_avg, ndvi],
+    #     axis=0,
+    # ).transpose()
+
+
     y = points["Classification"]
 
     data = Data(pos=pos, x=x, y=y, x_features_names=x_features_names)
