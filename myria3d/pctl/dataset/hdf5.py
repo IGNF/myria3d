c7783197769c2c420a69ce2c454e487bfc2b84ba
Charles Gaydon <charles.gaydon@gmail.com>
2023-11-16 13:33:15 +0100

    Add assert to avoid duplicated sample_number in hdf5

--- myria3d/pctl/dataset/hdf5.py
+++ myria3d/pctl/dataset/hdf5.py
@@ -278,6 +278,10 @@ def create_hdf5(
                         subtile_overlap,
                     )
                 ):
+                    if sample_number > 0:
+                        print("probleme")
+                        a = 2
+                        assert sample_number == 0
                     if not points_pre_transform:
                         continue
                     data = points_pre_transform(sample_points)
