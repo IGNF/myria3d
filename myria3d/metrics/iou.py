997b4b39e7c1adc8f09c30c932bcd67afd9f2773
Charles Gaydon <charles.gaydon@gmail.com>
2024-02-06 11:05:00 +0100

    refactor: follow python conventions of lowercase/uppercase use

--- myria3d/metrics/iou.py
+++ myria3d/metrics/iou.py
@@ -1,5 +1,7 @@
 from torch import Tensor
 
+EPSILON = 1e-8
+
 
 def iou(confmat: Tensor):
     """Computes the Intersection over Union of each class in the
@@ -9,9 +11,11 @@ def iou(confmat: Tensor):
         (iou, missing_class_mask) - iou for class as well as a mask
         highlighting existing classes
     """
-    TP_plus_FN = confmat.sum(dim=0)
-    TP_plus_FP = confmat.sum(dim=1)
-    TP = confmat.diag()
-    union = TP_plus_FN + TP_plus_FP - TP
-    iou = 1e-8 + TP / (union + 1e-8)
+    true_positives_and_false_negatives = confmat.sum(dim=0)
+    true_positives_and_false_positives = confmat.sum(dim=1)
+    true_positives = confmat.diag()
+    union = (
+        true_positives_and_false_negatives + true_positives_and_false_positives - true_positives
+    )
+    iou = EPSILON + true_positives / (union + EPSILON)
     return iou
