0be5fa2c89d9e9c3124108ceedc16adfb8306852
Lea Vauchier <lea.vauchier@ign.fr>
2024-03-11 18:38:03 +0100

    Fix edgecase with 1 input value that is not selected in subsample_data

--- tests/myria3d/models/test_model.py
+++ tests/myria3d/models/test_model.py
@@ -1,8 +1,9 @@
 import hydra
-from pytorch_lightning import LightningDataModule
+from pytorch_lightning import LightningDataModule, LightningModule
 from tests.conftest import make_default_hydra_cfg
 
 from myria3d.models.model import Model
+from myria3d.utils import utils  # noqa
 
 
 def test_model_get_batch_tensor_by_enumeration():
@@ -11,7 +12,7 @@ def test_model_get_batch_tensor_by_enumeration():
             "predict.src_las=tests/data/toy_dataset_src/862000_6652000.classified_toy_dataset.100mx100m.las",
             "datamodule.epsg=2154",
             "work_dir=./../../..",
-            "datamodule.subtile_width=1",
+            "datamodule.subtile_width=1",  # Extreme case with very few points per subtile
             "datamodule.hdf5_file_path=null",
         ]
     )
@@ -26,3 +27,27 @@ def test_model_get_batch_tensor_by_enumeration():
     for batch in datamodule.predict_dataloader():
         # Check that no error is raised ("TypeError: object of type 'numpy.int64' has no len()")
         _ = model._get_batch_tensor_by_enumeration(batch.idx_in_original_cloud)
+
+
+def test_model_forward():
+    config = make_default_hydra_cfg(
+        overrides=[
+            "predict.src_las=tests/data/toy_dataset_src/862000_6652000.classified_toy_dataset.100mx100m.las",
+            "datamodule.epsg=2154",
+            "work_dir=./../../..",
+            "datamodule.subtile_width=1",  # Extreme case with very few points per subtile
+            "datamodule.hdf5_file_path=null",
+        ]
+    )
+
+    datamodule: LightningDataModule = hydra.utils.instantiate(config.datamodule)
+    datamodule._set_predict_data(config.predict.src_las)
+
+    model: LightningModule = hydra.utils.instantiate(config.model)
+    device = utils.define_device_from_config_param(config.predict.gpus)
+    model.to(device)
+    model.eval()
+    print(model.model)
+    for batch in datamodule.predict_dataloader():
+        # Check that no error is raised
+        targets, logits = model.forward(batch)
