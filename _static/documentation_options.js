343424a356aa5ba1d0b04e6b8ae29ba0d446c514
leavauchier <leavauchier@users.noreply.github.com>
2026-05-11 15:06:02 +0000

    Deploying to gh-pages from @ IGNF/myria3d@36ef4b56fdf5269b48e06828acd20fc926b3fe47 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.8.4',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.9.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
