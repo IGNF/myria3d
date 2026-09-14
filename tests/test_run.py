5ab4bd69630410b81ed486a0a5359ea4e68f178c
Lea Vauchier <lea.vauchier@ign.fr>
2026-09-14 17:21:06 +0200

    Fix functional tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,9 +1,11 @@
 import runpy
 import sys
+from pathlib import Path
 
 import pytest
 
 pytestmark = pytest.mark.functional
+relative_output_dir = "tmp"
 
 
 @pytest.mark.parametrize(
@@ -25,9 +27,10 @@ def test_run_predict(monkeypatch, test_case, specific_args):
     """Test run.py execution when __name__ == "__main__" with mocked command line input."""
     test_args = [
         "run.py",
-        "predict.src_las=/inputs/792000_6272000_subset_buildings.laz",
-        f"predict.output_dir=/outputs/{test_case}",
+        "predict.src_las=${hydra:runtime.cwd}/tests/data/las_subset_buildings/792000_6272000_subset_buildings.laz",
+        "predict.output_dir=${hydra:runtime.cwd}" + f"/{relative_output_dir}/{test_case}",
         "task.task_name=predict",
     ] + specific_args
     monkeypatch.setattr(sys, "argv", test_args)
     runpy.run_module("run", run_name="__main__", alter_sys=True)
+    assert Path(f"{relative_output_dir}/{test_case}/792000_6272000_subset_buildings.laz").is_file()
